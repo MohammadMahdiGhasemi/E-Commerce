9b0f3bd6c51cf322bb62bb799f721ce1953a01e6
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2024-02-18 08:50:00 +0330

    update Users.urls

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path , include
 from . import views
+# from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 urlpatterns = [
     path('users/' , views.UserView.as_view(),name='user'),
     path('users/<int:pk>' , views.UserView.as_view(),name='user-pk'),
     path('address/' , views.AddressView.as_view(),name='address'),
     path('address/<int:pk>' , views.AddressView.as_view(),name='address-pk'),
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
